3b8800e52441cbf58f91d80709663f33ee064f81
reinh <Reinhardvandenbulcke@hotmail.com>
2025-04-30 10:56:35 +0200

    Reinhard Routes Added

--- Server.js
+++ Server.js
@@ -23,6 +23,53 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+// Stats
+app.get("/Stats", (req, res) => {
+  res.render("Stats");
+});
+
+// Login
+app.get("/Login", (req, res) => {
+  res.render("Login");
+});
+
+// Create Account
+app.get("/CreateAccount", (req, res) => {
+  res.render("CrAccount", {camps});
+});
+
+// Focus Mode
+app.get("/FocusMode", (req, res) => {
+  res.render("FocusMode");
+});
+
+// Settings
+app.get("/Settings", (req, res) => {
+  res.render("Settings");
+});
+
+// Leaderboard
+app.get("/Leaderboard", (req, res) => {
+  res.render("Leaderboard");
+});
+
+//Character Creation
+app.get("/CharacterCreation", (req, res) => {
+  res.render("CharacterCreation");
+});
+
+
+// Custom 404 page
+app.use((req, res) => {
+  res.render("errors/404");
+});
+
+// Custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.render("errors/500");
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
